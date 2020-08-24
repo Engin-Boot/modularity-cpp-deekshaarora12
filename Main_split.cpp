5f84baf28a4b8f77025008d008a33851c548d620
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-08-24 22:36:51 +0530

    Update Main_split.cpp

--- Main_split.cpp
+++ Main_split.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
-#include <assert.h>
-Using namespace TelCoColorCoder from Main.cpp;
-
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    int zeroBasedPairNumber = pairNumber - 1;
-    MajorColor majorColor =
-        (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    MinorColor minorColor =
-        (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return ColorPair(majorColor, minorColor);
-}
-int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-    return major * numberOfMinorColors + minor + 1;
-}
-}
+#include <Main.h>;
 
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
