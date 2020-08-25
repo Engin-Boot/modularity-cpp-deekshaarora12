2d105b2383e202d64a7413e99a9033aef4780cc1
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-08-25 13:46:40 +0530

    Update Main_split_2.cpp

--- Main_split_2.cpp
+++ Main_split_2.cpp
@@ -43,7 +43,7 @@ int main() {
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-    getManual()
+    getManual();
 
     return 0;
 }
