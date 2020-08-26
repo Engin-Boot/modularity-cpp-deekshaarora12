4cf7f3a1979d70d61a7bfde6d4b93f6aab75682f
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-08-26 19:13:30 +0530

    Update and rename Main_split_2.cpp to execution.cpp

---
+++ execution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<assert.h>
+#include "Main_split_2.h"
+
+
+int main() {
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    getManual();
+
+    return 0;
+}
