724214b85ad112d57db66c8e7325ee3eba1b7d18
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-08-26 19:17:27 +0530

    Update execution.cpp

--- execution.cpp
+++ execution.cpp
@@ -2,6 +2,30 @@
 #include<assert.h>
 #include "Main_split_2.h"
 
+int computePairNumber(int majcol, int mincol) {
+    return majcol * TelCoColorCoder::numberOfMinorColors + mincol + 1;
+}
+
+void getManual() {
+
+    string Manual = "";
+  
+    for (int majorColor_itr = 0; majorColor_itr < TelCoColorCoder::numberOfMajorColors; majorColor_itr++) {
+        Manual = "";
+        for (int minorColor_itr = 0; minorColor_itr < TelCoColorCoder::numberOfMinorColors; minorColor_itr++) {
+            string majorcolor_name = TelCoColorCoder::MajorColorNames[majorColor_itr];
+            string minorcolor_name = TelCoColorCoder::MinorColorNames[minorColor_itr];
+            int pairNumber = computePairNumber(majorColor_itr, minorColor_itr);
+
+            string pair_number_to_string = std::to_string(pairNumber);
+
+            Manual = Manual + pair_number_to_string + " " + majorcolor_name + " " + minorcolor_name + "\n";
+
+       }
+        cout << Manual << endl;
+    }   
+}
+
 
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
